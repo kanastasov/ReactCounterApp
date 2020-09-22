1ea6674ab63f9b863c040912d5cee94f1a72bb35
kanastasoff <kireprogramista@gmail.com>
2020-09-22 17:22:27 +0300

    Add navbar and input rules

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,50 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
- state = {
-     count:0,
-     tags: ["item1", "item2", "item3"],
-     product: ''
-   
- };
+  render() {
+    return (
+      <div className="row">
+        <div className="col-1">
+          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        </div>
+        <div className="col">
+          <button
+            onClick={() => this.props.onIncrement(this.props.counter)}
+            className="btn btn-secondary btn-sm"
+          >
+            +
+          </button>
+          <button
+            onClick={() => this.props.onDecrement(this.props.counter)}
+            className="btn btn-secondary btn-sm m-2"
+            disabled={this.props.counter.value === 0 ? "disabled" : ""}
+          >
+            -
+          </button>
+          <button
+            onClick={() => this.props.onDelete(this.props.counter.id)}
+            className="btn btn-danger btn-sm"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    );
+  }
 
- constructor() {
-     super();
-     this.handleIncrement(this);
-     console.log('Constructor', this);
- }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
 
-    handleIncrement = () => {
-        this.setState({ count: this.state.count + 1});
-        console.log('handleIncrement');
-    };
-
-    renderTags() {
-        if(this.state.tags.length === 0) return <p>There are not tags</p>;
-
-    return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
-    };
- 
-    render() {    
-    return(
-    <div>
-        {/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span> */}
-        <button onClick={() => this.handleIncrement()}
-        className="btn btn-secondary btn-sm">
-        Increment
-        </button>
-        <ul>
-            <li>{this.state.count}</li>
-
-        </ul>
-       {this.state.tags.length === 0 && "Please create new tag!"}
-       {this.renderTags()}
-          
-    </div>); 
- 
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
+  }
 }
 
-
-}
- 
-export default Counter; 
+export default Counter;
